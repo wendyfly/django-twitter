17cf14662d055424bf653f50d40c13c4b2c94c80
Wenwen Liao <wenwenliao@Wenwens-MacBook-Pro.local>
2021-05-04 09:57:02 -0700

    create tweet api

--- tweets/api/views.py
+++ tweets/api/views.py
@@ -1,13 +1,13 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from tweets.api.serializers import TweetSerializer
+from tweets.api.serializers import TweetSerializer, TweetCreateSerializer
 from tweets.models import Tweet
 
 
 # note: try to avoid to use modelviewset, because we don't want it have delete/update action by default
 class TweetViewSet(viewsets.GenericViewSet, ):
-    serializer_class = TweetSerializer
+    serializer_class = TweetCreateSerializer
 
     def get_permissions(self):
         if self.action == 'list':
@@ -24,10 +24,24 @@ class TweetViewSet(viewsets.GenericViewSet, ):
         # 这句 SQL 查询会用到 user 和 created_at 的联合索引
         # 单纯的 user 索引是不够的
         tweets = Tweet.objects.filter(user_id=request.query_params['user_id']
-        ).order_by('-created_at')
-        #many = true means it will return a list of dict
+                                      ).order_by('-created_at')
+        # many = true means it will return a list of dict
         serializer = TweetSerializer(tweets, many=True)
         return Response({'tweets': serializer.data})
 
-    def create(self, request):
-        pass
+    def create(self, request, *args, **kwargs):
+        """
+        重载 create 方法，因为需要默认用当前登录用户作为 tweet.user
+        """
+        serializer = TweetCreateSerializer(
+            data=request.data,
+            context={'request': request},
+        )
+        if not serializer.is_valid():
+            return Response({
+                'success': False,
+                'message': "Please check input",
+                'errors': serializer.errors,
+            }, status=400)
+        tweet = serializer.save()
+        return Response(TweetSerializer(tweet).data, status=201)
