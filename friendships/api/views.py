5289f32f707278b507ce15872150b5eed65600ac
Wenwen Liao <wenwenliao@Wenwens-MacBook-Pro.local>
2021-05-07 20:13:28 -0700

    follow and unfollow apis

--- friendships/api/views.py
+++ friendships/api/views.py
@@ -6,11 +6,13 @@ from friendships.models import Friendship
 from friendships.api.serializers import (
     FollowerSerializer,
     FollowingSerializer,
+    FriendshipSerializerForCreate,
 )
 from django.contrib.auth.models import User
 
 
 class FriendshipViewSet(viewsets.GenericViewSet):
+    serializer_class = FriendshipSerializerForCreate
     # 我们希望 POST /api/friendship/1/follow 是去 follow user_id=1 的用户
     # 因此这里 queryset 需要是 User.objects.all()
     # 如果是 Friendship.objects.all 的话就会出现 404 Not Found
@@ -28,6 +30,7 @@ class FriendshipViewSet(viewsets.GenericViewSet):
             status=status.HTTP_200_OK
         )
 
+    # detail = true means this function will be part of api
     @action(methods=['GET'], detail=True, permission_classes=[AllowAny])
     def followings(self, request, pk):
         friendships = Friendship.objects.filter(from_user_id= pk).order_by('-created_at')
@@ -36,6 +39,38 @@ class FriendshipViewSet(viewsets.GenericViewSet):
             {'followings': serializer.data},
             status=status.HTTP_200_OK
         )
+
+    @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
+    # /api/friendships/<pk>/follow
+    def follow(self, request, pk):
+
+        serializer = FriendshipSerializerForCreate(data= {
+            'from_user_id': request.user.id,
+            'to_user_id': pk,
+        })
+        if not serializer.is_valid():
+            return Response({
+                "success": False,
+                "message": "Please check input.",
+                "errors": serializer.errors,
+            }, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'success': True}, status=status.HTTP_201_CREATED)
+
+    @action(methods=['POST'], detail=True, permission_classes=[IsAuthenticated])
+    # /api/friendships/<pk>/unfollow
+    def unfollow(self, request, pk):
+        # using pk as id
+        unfollow_user = self.get_object()
+        if request.user.id == unfollow_user.id:
+            return Response({
+                'success': False,
+                'message': 'You cannot unfollow yourself'
+            }, status=status.HTTP_400_BAD_REQUEST)
+        deleted, _ =  Friendship.objects.filter(
+            from_user=request.user,
+            to_user=pk,
+        ).delete()
+        return Response({'success': True, 'deleted': deleted}, status=status.HTTP_204_NO_CONTENT)
 
     def list(self, request):
         return Response({'message': 'this is placeholder'})
