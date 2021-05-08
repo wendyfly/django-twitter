9619d1a49e829b6b04936346ebc2b7602432f09a
Wenwen Liao <wenwenliao@Wenwens-MacBook-Pro.local>
2021-05-08 14:11:57 -0700

    add newsfeed model

---
+++ newsfeeds/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import  User
+from tweets.models import Tweet
+
+class NewsFeed(models.Model):
+    # this user is the user who can view this tweet
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    tweet = models.ForeignKey(Tweet, on_delete=models.SET_NULL, null = True)
+    # need created_at field to help sort by -created_date
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        index_together=(('user', 'created_at'),)
+        unique_together = (('user', 'tweet'),)
+        ordering = ('user', 'created_at',)
+
+    def __str__(self):
+        return f'{self.created_at} inbox of {self.user}: {self.tweet}'
