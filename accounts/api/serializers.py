1ad08ecbd4185fda9198abb2644f2b0ebbc12b1b
Wenwen Liao <wenwenliao@Wenwens-MacBook-Pro.local>
2021-06-01 23:17:22 -0700

    inject likes to other apis

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -14,6 +14,10 @@ class UserSerializerForTweet(serializers.ModelSerializer):
         fields = ['id', 'username']
 
 
+class UserSerializerForLike(UserSerializerForTweet):
+    pass
+
+
 # inheritance from UserSerializerForTweet
 class UserSerializerForComments(UserSerializerForTweet):
     pass
