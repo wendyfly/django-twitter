5166aecd85d08a8a47334f4e41ef142e9157b965
Wenwen Liao <wenwenliao@Wenwens-MacBook-Pro.local>
2021-07-04 23:40:07 -0700

    cache tweets in redis

--- tweets/services.py
+++ tweets/services.py
@@ -1,4 +1,7 @@
 from tweets.models import TweetPhoto
+from tweets.models import Tweet
+from twitter.cache import USER_TWEETS_PATTERN
+from utils.redis_helper import RedisHelper
 
 
 class TweetService(object):
@@ -15,3 +18,17 @@ class TweetService(object):
             )
             photos.append(photo)
         TweetPhoto.objects.bulk_create(photos)
+
+    @classmethod
+    def get_cached_tweets(cls, user_id):
+        # queryset is lazy loading, it will loaded when it's needed. eg, for loop queryset, list(queryset)
+        queryset = Tweet.objects.filter(user_id=user_id).order_by('-created_at')
+        key = USER_TWEETS_PATTERN.format(user_id=user_id)
+        return RedisHelper.load_objects(key, queryset)
+
+    @classmethod
+    def push_tweet_to_cache(cls, tweet):
+        # queryset is lazy loading
+        queryset = Tweet.objects.filter(user_id=tweet.user_id).order_by('-created_at')
+        key = USER_TWEETS_PATTERN.format(user_id=tweet.user_id)
+        RedisHelper.push_object(key, tweet, queryset)
