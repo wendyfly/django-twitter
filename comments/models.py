86fd425a20696f5a9225c0cf7c3d8025b29fa857
Wenwen Liao <wenwenliao@Wenwens-MacBook-Pro.local>
2021-07-09 23:18:31 -0700

    denormalize the count data

--- comments/models.py
+++ comments/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import User
 from tweets.models import Tweet
 from likes.models import Like
 from django.contrib.contenttypes.models import ContentType
-from accounts.services import UserService
+from comments.listeners import incr_comments_count, decr_comments_count
 from utils.memcached_helper import MemcachedHelper
+from django.db.models.signals import post_save, pre_delete
 
 
 class Comment(models.Model):
@@ -39,3 +40,6 @@ class Comment(models.Model):
     @property
     def cached_user(self):
         return MemcachedHelper.get_object_through_cache(User, self.user_id)
+
+post_save.connect(incr_comments_count, sender=Comment)
+pre_delete.connect(decr_comments_count, sender=Comment)
