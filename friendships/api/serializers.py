886c7fedf053916627f33a752dba31caaa4e30b6
Wenwen Liao <wenwenliao@Wenwens-MacBook-Pro.local>
2021-08-19 15:08:12 -0700

    hbase friendship serializer and zookeeper

--- friendships/api/serializers.py
+++ friendships/api/serializers.py
@@ -4,12 +4,24 @@ from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from django.contrib.auth.models import User
 from friendships.services import FriendshipService
+from accounts.services import UserService
 
 
-class FollowingUserIdSetMixin:
+class BaseFriendshipSerializer(serializers.Serializer):
+    user = serializers.SerializerMethodField()
+    created_at = serializers.SerializerMethodField()
+    has_followed = serializers.SerializerMethodField()
 
-    @property
-    def following_user_id_set(self: serializers.ModelSerializer):
+    def update(self, instance, validated_data):
+        pass
+
+    def create(self, validated_data):
+        pass
+
+    def get_user_id(self, obj):
+        raise NotImplementedError
+
+    def _get_following_user_id_set(self):
         if self.context['request'].user.is_anonymous:
             return {}
         # if it's in the cache
@@ -21,34 +33,25 @@ class FollowingUserIdSetMixin:
         setattr(self, '_cached_following_user_id_set', user_id_set)
         return user_id_set
 
-
-class FollowerSerializer(serializers.ModelSerializer, FollowingUserIdSetMixin):
-    user = UserSerializerForFriendship(source='cached_from_user')
-    created_at = serializers.DateTimeField()
-    has_followed = serializers.SerializerMethodField()
-
-    # if created_at is not in the above fields, it will try to find it in the model
-    class Meta:
-        model = Friendship
-        fields = ('user', 'created_at', 'has_followed')
-
     def get_has_followed(self, obj):
-        return obj.from_user_id in self.following_user_id_set
+        return self.get_user_id(obj) in self._get_following_user_id_set()
+
+    def get_user(self, obj):
+        user = UserService.get_user_by_id(self.get_user_id(obj))
+        return UserSerializerForFriendship(user).data
+
+    def get_created_at(self, obj):
+        return obj.created_at
 
 
-class FollowingSerializer(serializers.ModelSerializer, FollowingUserIdSetMixin):
-    user = UserSerializerForFriendship(source='cached_to_user')
-    created_at = serializers.DateTimeField()
-    has_followed = serializers.SerializerMethodField()
+class FollowerSerializer(BaseFriendshipSerializer):
+        def get_user_id(self, obj):
+            return obj.from_user_id
 
 
-    # if created_at is not in the above fields, it will try to find it in the model
-    class Meta:
-        model = Friendship
-        fields = ('user', 'created_at', 'has_followed')
-
-    def get_has_followed(self, obj):
-       return obj.to_user_id in self.following_user_id_set
+class FollowingSerializer(BaseFriendshipSerializer):
+    def get_user_id(self, obj):
+        return obj.to_user_id
 
 
 class FriendshipSerializerForCreate(serializers.ModelSerializer):
@@ -81,7 +84,7 @@ class FriendshipSerializerForCreate(serializers.ModelSerializer):
         return attrs
 
     def create(self, validated_data):
-        return Friendship.objects.create(
+        return FriendshipService.follow(
             from_user_id=validated_data['from_user_id'],
             to_user_id=validated_data['to_user_id'],
         )
